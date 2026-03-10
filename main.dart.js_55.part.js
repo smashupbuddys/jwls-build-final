d92cfc822c85664163a25ed0db50d9f8b2256dad
smashupbuddys <68978773+smashupbuddys@users.noreply.github.com>
2026-03-10 16:31:04 +0530

    unzipgit

---
+++ main.dart.js_55.part.js
@@ -0,0 +1,166 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,C,A={
+fPZ(){return new A.abi(null)},
+abi:function abi(d){this.a=d},
+aUX:function aUX(d){var _=this
+_.d=d
+_.e=!0
+_.c=_.a=null},
+dvx:function dvx(d){this.a=d},
+dvw:function dvw(d,e,f){this.a=d
+this.b=e
+this.c=f},
+dvv:function dvv(d,e){this.a=d
+this.b=e},
+dvz:function dvz(d){this.a=d},
+dvy:function dvy(d){this.a=d},
+dvA:function dvA(d){this.a=d},
+dvB:function dvB(){},
+dvC:function dvC(){},
+dvD:function dvD(d){this.a=d},
+dvE:function dvE(){},
+dvF:function dvF(){},
+dvG:function dvG(d){this.a=d},
+dvH:function dvH(d){this.a=d},
+dvI:function dvI(d){this.a=d},
+NC:function NC(d,e,f,g,h,i,j){var _=this
+_.a=d
+_.b=e
+_.c=f
+_.d=g
+_.e=h
+_.f=i
+_.r=j}},D
+B=c[0]
+C=c[2]
+A=a.updateHolder(c[21],A)
+D=c[29]
+A.abi.prototype={
+R(){return new A.aUX(B.a([],y.q))}}
+A.aUX.prototype={
+a_(){this.a4()
+this.W1()},
+W1(){var x=0,w=B.C(y.r),v=this
+var $async$W1=B.y(function(d,e){if(d===1)return B.z(e,w)
+for(;;)switch(x){case 0:x=2
+return B.l(B.eV(C.h3,null,y.b),$async$W1)
+case 2:v.m(new A.dvx(v))
+return B.A(null,w)}})
+return B.B($async$W1,w)},
+c4s(d,e){this.m(new A.dvw(this,d,e))},
+c4_(d,e){e=e.toLowerCase()
+return C.o.u(d.b.toLowerCase(),e)||C.o.u(d.c.toLowerCase(),e)||C.o.u(d.d.toLowerCase(),e)||C.o.u(d.r.toLowerCase(),e)},
+D(d){var x,w,v,u,t,s=this,r=null,q=$.bj,p=B.bn(q)
+q=B.u(d).ax.a===C.r?C.m6:C.h
+x=p.a
+w=y.u
+v=B.ah2(B.a([B.b5(r,r,r,C.nD,r,r,new A.dvz(s),r,r,r,"Refresh Data",r),B.b5(r,r,r,C.tW,r,r,new A.dvA(d),r,r,r,"Export to Excel",r)],w),r,!0,x,r,r,C.h,r,r,r,r,D.dTE)
+u=3*$.w.i()/100
+t=y.E
+return B.Nm(v,q,new B.U(new B.x(u,u,u,u),B.p(B.a([B.c("Product Inventory",r,r,r,r,r,r,r,r,r,B.t(r,r,x,r,r,r,r,r,r,r,r,B.X(20),r,r,C.x,r,r,!0,r,r,r,r,r,r,r,r),r,r,r,r,r,r),B.j(r,$.D.i()/100,r),B.c("Excel-style data table with sorting, search, and pagination",r,r,r,r,r,r,r,r,r,B.t(r,r,C.aw,r,r,r,r,r,r,r,r,B.X(11),r,r,r,r,r,!0,r,r,r,r,r,r,r,r),r,r,r,r,r,r),B.j(r,3*$.D.i()/100,r),B.v(B.frl(B.a([new B.TP("Product Name","name",new A.dvB(),t),new B.TP("SKU","sku",new A.dvC(),t),new B.TP("Category","category",new A.dvD(p),t),new B.TP("Price","price",new A.dvE(),t),new B.TP("Stock","stock",new A.dvF(),t),new B.TP("Status","status",new A.dvG(s),t)],y.C),s.d,!0,s.e,new A.dvH(d),s.gc3Z(),new A.dvI(d),s.gc4r(),!0,10,!0,!0,!0,y.k),1)],w),C.u,r,C.b,C.c,0,C.k),r),r,r)},
+bOB(d){var x,w,v,u=null
+switch(d){case"In Stock":x=C.J.j(0.1)
+w=C.J
+break
+case"Low Stock":x=C.a2.j(0.1)
+w=C.a2
+break
+case"Out of Stock":x=C.K.j(0.1)
+w=C.K
+break
+default:x=C.aK.j(0.1)
+w=C.aK}v=B.f(12)
+return B.i(u,B.c(d,u,u,u,u,u,u,u,u,u,B.t(u,u,w,u,u,u,u,u,u,u,u,B.X(10),u,u,C.v,u,u,!0,u,u,u,u,u,u,u,u),u,u,u,u,u,u),C.e,u,u,new B.k(x,u,u,v,u,u,u,C.j),u,u,u,u,C.nA,u,u,u)}}
+A.NC.prototype={
+gd1(d){return this.a},
+gaV(d){return this.b}}
+var z=a.updateTypes(["O(NC)","~(G,an)","an(NC,G)","W(NC,NC)","ef(NC)","I(NC)","c3(NC)","c3(ai<NC>)"])
+A.dvx.prototype={
+$0(){var x="Rings",w="In Stock",v="Earrings",u="Low Stock",t=this.a
+t.d=B.a([new A.NC("1","Diamond Ring","DR-001",x,2499.99,12,w),new A.NC("2","Gold Necklace","GN-002","Necklaces",1899.5,8,w),new A.NC("3","Silver Bracelet","SB-003","Bracelets",349.99,25,w),new A.NC("4","Pearl Earrings","PE-004",v,599,15,w),new A.NC("5","Platinum Watch","PW-005","Watches",4999.99,3,u),new A.NC("6","Ruby Pendant","RP-006","Pendants",1299.99,0,"Out of Stock"),new A.NC("7","Emerald Brooch","EB-007","Brooches",899.5,6,w),new A.NC("8","Sapphire Ring","SR-008",x,3299,10,w),new A.NC("9","White Gold Chain","WGC-009","Chains",799.99,20,w),new A.NC("10","Diamond Studs","DS-010",v,1599,14,w),new A.NC("11","Gold Bangle","GB-011","Bangles",1199.5,18,w),new A.NC("12","Silver Anklet","SA-012","Anklets",249.99,22,w),new A.NC("13","Rose Gold Ring","RGR-013",x,1899,2,u),new A.NC("14","Cubic Zirconia Set","CZS-014","Sets",699.99,30,w),new A.NC("15","Titanium Band","TB-015","Bands",399.5,40,w)],y.q)
+t.e=!1},
+$S:0}
+A.dvw.prototype={
+$0(){C.m.e7(this.a.d,new A.dvv(this.b,this.c))},
+$S:0}
+A.dvv.prototype={
+$2(d,e){var x,w
+switch(this.a){case"name":x=d.b
+w=e.b
+break
+case"sku":x=d.c
+w=e.c
+break
+case"category":x=d.d
+w=e.d
+break
+case"price":x=d.e
+w=e.e
+break
+case"stock":x=d.f
+w=e.f
+break
+case"status":x=d.r
+w=e.r
+break
+default:return 0}if(typeof x=="string"&&typeof w=="string")return this.b?C.o.aH(x,w):C.o.aH(w,x)
+else if(typeof x=="number"&&typeof w=="number")return this.b?C.p.aH(x,w):C.p.aH(w,x)
+return 0},
+$S:z+3}
+A.dvz.prototype={
+$0(){var x=this.a
+x.m(new A.dvy(x))
+x.W1()},
+$S:0}
+A.dvy.prototype={
+$0(){return this.a.e=!0},
+$S:0}
+A.dvA.prototype={
+$0(){this.a.C(y.v).f.L(D.dt9)},
+$S:0}
+A.dvB.prototype={
+$1(d){var x=null
+return B.c(d.b,x,x,x,x,x,x,x,x,x,B.t(x,x,x,x,x,x,x,x,x,x,x,B.X(11),x,x,C.v,x,x,!0,x,x,x,x,x,x,x,x),x,x,x,x,x,x)},
+$S:z+0}
+A.dvC.prototype={
+$1(d){var x=null,w=B.X(10)
+return B.c(d.c,x,x,x,x,x,x,x,x,x,B.t(x,x,$.N?C.L:C.N,x,x,x,x,x,"JetBrains Mono",x,x,w,x,x,x,x,x,!0,x,x,x,x,x,x,x,x),x,x,x,x,x,x)},
+$S:z+0}
+A.dvD.prototype={
+$1(d){var x=null,w=this.a.a,v=w.j(0.1),u=B.f(4)
+return B.i(x,B.c(d.d,x,x,x,x,x,x,x,x,x,B.t(x,x,w,x,x,x,x,x,x,x,x,B.X(10),x,x,C.R,x,x,!0,x,x,x,x,x,x,x,x),x,x,x,x,x,x),C.e,x,x,new B.k(v,x,x,u,x,x,x,C.j),x,x,x,x,C.cY,x,x,x)},
+$S:z+4}
+A.dvE.prototype={
+$1(d){var x=null
+return B.c(B.bk(d.e,x),x,x,x,x,x,x,x,x,x,B.t(x,x,C.J,x,x,x,x,x,x,x,x,B.X(11),x,x,C.v,x,x,!0,x,x,x,x,x,x,x,x),x,x,x,x,x,x)},
+$S:z+0}
+A.dvF.prototype={
+$1(d){var x,w=null,v=d.f,u=B.X(11)
+if(v===0)x=C.K
+else x=v<5?C.a2:C.J
+return B.c(""+v,w,w,w,w,w,w,w,w,w,B.t(w,w,x,w,w,w,w,w,w,w,w,u,w,w,C.v,w,w,!0,w,w,w,w,w,w,w,w),w,w,w,w,w,w)},
+$S:z+0}
+A.dvG.prototype={
+$1(d){return this.a.bOB(d.r)},
+$S:z+5}
+A.dvH.prototype={
+$1(d){var x=null
+this.a.C(y.v).f.L(B.aw(x,x,x,x,x,C.A,x,B.c("Tapped: "+d.b,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x),x,C.Q,x,x,x,x,x,x,x,x,x,x))},
+$S:z+6}
+A.dvI.prototype={
+$1(d){var x=null
+this.a.C(y.v).f.L(B.aw(x,x,x,x,x,C.A,x,B.c(""+d.length+" items selected",x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x),x,C.Q,x,x,x,x,x,x,x,x,x,x))},
+$S:z+7};(function installTearOffs(){var x=a._instance_2u
+var w
+x(w=A.aUX.prototype,"gc4r","c4s",1)
+x(w,"gc3Z","c4_",2)})();(function inheritance(){var x=a.inherit,w=a.inheritMany
+x(A.abi,B.am)
+x(A.aUX,B.al)
+w(B.T5,[A.dvx,A.dvw,A.dvz,A.dvy,A.dvA])
+x(A.dvv,B.T6)
+w(B.Qs,[A.dvB,A.dvC,A.dvD,A.dvE,A.dvF,A.dvG,A.dvH,A.dvI])
+x(A.NC,B.aG)})()
+B.Xc(b.typeUniverse,JSON.parse('{"abi":{"am":[],"I":[],"o":[]},"aUX":{"al":["abi"],"o":[]}}'))
+var y={E:B.b0("TP<NC>"),C:B.b0("a9<TP<NC>>"),q:B.b0("a9<NC>"),u:B.b0("a9<I>"),k:B.b0("NC"),v:B.b0("V3"),b:B.b0("@"),r:B.b0("~")};(function constants(){D.dt9=new B.c1(C.azn,null,null,null,null,null,null,null,null,null,null,null,null,C.Q,!1,null,null,null,C.A,null)
+D.dTE=new B.O("Excel Data Table Demo",null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null)})()};
+(a=>{a["wNMfA4lDHvZV0mXmsaHSb0vDSxY="]=a.current})($__dart_deferred_initializers__);
